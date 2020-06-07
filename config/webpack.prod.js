9c331a667268056e75fea1d8cb8a3fe830be9262
huyinglin <fazheng@indata.cc>
2020-06-08 00:04:10 +0800

    feat: dupate

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,14 +1,30 @@
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const prodConfig = {
-	mode: 'production',
-	devtool: 'cheap-module-source-map',
-	optimization: {
-		minimizer: [new OptimizeCSSAssetsPlugin({
-			assetNameRegExp: /\.css$/g,
-			cssProcessor: require('cssnano')
-		})] // css压缩
-	},
-}
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [{
+      test: /\.less$/,
+      use: [
+        {
+          loader: 'style-loader',
+          options: {
+            inserAt: 'top', // 样式插入到<head>
+            singleton: true, // 将所有到style标签合并成一个
+          },
+        },
+        'css-loader',
+        'less-loader', 
+      ],
+    }],
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+    })], // css压缩
+  },
+};
 
 module.exports = prodConfig;
